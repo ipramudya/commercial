2bb0914f00a3d90a98da5db20ded92299bf2b051
ipramudya <ipramudya.dev@gmail.com>
2024-07-21 15:36:34 +0700

    init project

---
+++ src/app/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as CompatLogo } from "./logo-01.png";
+export { default as WideLogo } from "./logo-02.png";
+
+export { default as SamplePartnerPic1 } from "./ourpartnersample-01.png";
+export { default as SamplePartnerPic2 } from "./ourpartnersample-02.png";
+export { default as SamplePartnerPic3 } from "./ourpartnersample-03.png";
+export { default as SamplePartnerPic4 } from "./ourpartnersample-04.png";
