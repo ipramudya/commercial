e3daa0108e49d28dcbd8f0099ee0d93223ca864f
ipramudya <ipramudya.dev@gmail.com>
2024-07-25 15:39:35 +0700

    add scale on hover product item

--- src/app/products/ProductItem.tsx
+++ src/app/products/ProductItem.tsx
@@ -22,7 +22,11 @@ export default function ProductItem() {
                 onClick={() => onOpenChange(true)}
             >
                 <div className="aspect-square w-full overflow-hidden rounded-2xl">
-                    <Image src={CustomImg} alt="products" className="h-auto w-full" />
+                    <Image
+                        src={CustomImg}
+                        alt="products"
+                        className="h-auto w-full transition-all duration-300 hover:scale-105"
+                    />
                 </div>
                 <p className="text-center font-semibold">Modern Chair</p>
             </div>
