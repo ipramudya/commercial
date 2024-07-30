900547662f6ded653d53888542e8cb2eefd2ccff
ipramudya <ipramudya.dev@gmail.com>
2024-07-30 17:48:20 +0700

    add smooth scrolling

--- src/app/(landing)/About.tsx
+++ src/app/(landing)/About.tsx
@@ -1,37 +1,46 @@
+"use client";
+
 import { AboutImg, WideLogo } from "@/assets/images";
 import { WeenIndokraftMeaningTeks, WeenIndokraftTeks } from "@/static/contents";
 import Image from "next/image";
+import { Element as ScrollElement } from "react-scroll";
 
 export default function About() {
     return (
-        <section className="flex flex-col px-6 py-[3rem] xl:mx-auto xl:w-full xl:max-w-screen-xl xl:p-0 xl:py-[6rem]">
-            <div className="grid grid-cols-1 gap-[3rem] md:grid-cols-2">
-                <div className="flex flex-col items-center justify-center space-y-6 xl:space-y-[3rem]">
-                    <div className="relative w-[300px]">
-                        <Image
-                            src={WideLogo}
-                            alt="ween-indokraft-about"
-                            className="h-auto w-full"
-                            quality={100}
-                        />
-                    </div>
+        <>
+            <ScrollElement name="ween">
+                <section className="flex flex-col px-6 py-[3rem] xl:mx-auto xl:w-full xl:max-w-screen-xl xl:p-0 xl:py-[6rem]">
+                    <div className="grid grid-cols-1 gap-[3rem] md:grid-cols-2">
+                        <div className="flex flex-col items-center justify-center space-y-6 xl:space-y-[3rem]">
+                            <div className="relative w-[300px]">
+                                <Image
+                                    src={WideLogo}
+                                    alt="ween-indokraft-about"
+                                    className="h-auto w-full"
+                                    quality={100}
+                                />
+                            </div>
 
-                    <p className="max-w-[500px] text-sm leading-normal text-neutral-700 lg:max-w-[380px]">
-                        <strong>{WeenIndokraftTeks}</strong> {WeenIndokraftMeaningTeks}
-                    </p>
-                </div>
+                            <ScrollElement name="ween-indokraft-about">
+                                <p className="max-w-[500px] text-sm leading-normal text-neutral-700 lg:max-w-[380px]">
+                                    <strong>{WeenIndokraftTeks}</strong> {WeenIndokraftMeaningTeks}
+                                </p>
+                            </ScrollElement>
+                        </div>
 
-                <div className="flex items-center justify-center">
-                    <div className="flex h-full w-full items-center justify-center lg:max-w-[500px]">
-                        <Image
-                            src={AboutImg}
-                            alt="ween-indokraft-about-pic"
-                            className="h-auto w-full"
-                            quality={100}
-                        />
+                        <div className="flex items-center justify-center">
+                            <div className="flex h-full w-full items-center justify-center lg:max-w-[500px]">
+                                <Image
+                                    src={AboutImg}
+                                    alt="ween-indokraft-about-pic"
+                                    className="h-auto w-full"
+                                    quality={100}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </section>
+                </section>
+            </ScrollElement>
+        </>
     );
 }
--- src/app/(landing)/Banner.tsx
+++ src/app/(landing)/Banner.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { ChevLeftIcon, ChevRightIcon } from "@/components/Icon";
-import useEmblaCarousel from "embla-carousel-react";
+import { useRouter } from "next/navigation";
+import { Link } from "react-scroll";
 
 export default function Banner() {
-    const [emblaRef, emblaAPI] = useEmblaCarousel();
+    const router = useRouter();
 
     return (
         <div className="relative flex h-[calc(80dvh-100px)] w-full items-center justify-center overflow-hidden lg:h-[calc(100dvh-120px)]">
             <div className="absolute left-0 top-0 z-10 h-full w-full bg-black/30" />
 
-            <div className="z-[1] h-full w-full" ref={emblaRef}>
+            <div className="z-[1] h-full w-full">
                 <div className="flex h-full">
                     <div className="min-w-0 flex-shrink-0 flex-grow-0 basis-full">
                         <video
@@ -33,32 +33,16 @@ export default function Banner() {
 
             <div className="absolute left-0 top-0 z-10 h-full w-full">
                 <div className="relative z-10 flex h-full items-center justify-center">
-                    <button
-                        type="button"
-                        className="absolute left-2 top-1/2 -translate-y-1/2 md:left-[2%]"
-                        onClick={() => {
-                            if (emblaAPI) emblaAPI.scrollPrev();
-                        }}
-                    >
-                        <ChevLeftIcon width={38} height={38} className="stroke-white" />
-                    </button>
-
-                    <div className="flex w-full max-w-screen-lg flex-col space-y-2 px-[3rem] md:px-[5rem] xl:max-w-screen-xl xl:space-y-4 2xl:px-0">
-                        <h2 className="text-3xl font-bold text-white md:text-4xl lg:text-5xl">
+                    <div className="flex w-full max-w-screen-lg flex-col items-center space-y-2 px-[3rem] md:px-[5rem] xl:max-w-screen-xl xl:space-y-4 2xl:px-0">
+                        <h2 className="text-center text-3xl font-bold text-white md:text-4xl lg:text-5xl">
                             Memenuhi setiap <br /> sudut rumah anda.
                         </h2>
-                        <p className="font-semibold text-white lg:text-lg">Lihat lebih lanjut</p>
+                        <Link to="ween" smooth duration={500}>
+                            <span className="cursor-pointer font-semibold text-white lg:text-lg">
+                                Lihat lebih lanjut
+                            </span>
+                        </Link>
                     </div>
-
-                    <button
-                        type="button"
-                        className="absolute right-2 top-1/2 -translate-y-1/2 md:right-[2%]"
-                        onClick={() => {
-                            if (emblaAPI) emblaAPI.scrollNext();
-                        }}
-                    >
-                        <ChevRightIcon width={38} height={38} className="stroke-white" />
-                    </button>
                 </div>
             </div>
         </div>
