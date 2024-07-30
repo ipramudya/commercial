bfb4a1c3d1ab29808e44cbac4dedb22cccbd796d
ipramudya <ipramudya.dev@gmail.com>
2024-07-30 16:16:11 +0700

    gausa slideshow carousel

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -5,7 +5,7 @@ import OurWorks from "./OurWorks";
 export default function Page() {
     return (
         <Fragment>
-            <CenterContentsBanner title="Projects" description="Our latest projects" />
+            <CenterContentsBanner title="Projects" description="Our latest projects" disabled />
 
             <OurWorks />
         </Fragment>
--- src/components/CenterContentsBanner.tsx
+++ src/components/CenterContentsBanner.tsx
@@ -8,10 +8,11 @@ import Image from "next/image";
 interface Props {
     title: string;
     description: string;
+    disabled?: boolean;
 }
 
-export default function CenterContentsBanner({ title, description }: Props) {
-    const [emblaRef, emblaAPI] = useEmblaCarousel();
+export default function CenterContentsBanner({ title, description, disabled = false }: Props) {
+    const [emblaRef, emblaAPI] = useEmblaCarousel({ active: !disabled });
 
     return (
         <div className="relative h-[calc(80dvh-100px)] w-full overflow-hidden lg:h-[calc(100dvh-120px)]">
@@ -36,15 +37,17 @@ export default function CenterContentsBanner({ title, description }: Props) {
                 </div>
             </div>
 
-            <button
-                type="button"
-                className="absolute left-2 top-1/2 z-10 -translate-y-1/2 md:left-[2%]"
-                onClick={() => {
-                    if (emblaAPI) emblaAPI.scrollPrev();
-                }}
-            >
-                <ChevLeftIcon width={38} height={38} className="stroke-white" />
-            </button>
+            {!disabled && (
+                <button
+                    type="button"
+                    className="absolute left-2 top-1/2 z-10 -translate-y-1/2 md:left-[2%]"
+                    onClick={() => {
+                        if (emblaAPI) emblaAPI.scrollPrev();
+                    }}
+                >
+                    <ChevLeftIcon width={38} height={38} className="stroke-white" />
+                </button>
+            )}
 
             <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
                 <div className="flex flex-col items-center justify-center space-y-3">
@@ -58,15 +61,17 @@ export default function CenterContentsBanner({ title, description }: Props) {
                 </div>
             </div>
 
-            <button
-                type="button"
-                className="absolute right-2 top-1/2 z-10 -translate-y-1/2 md:right-[2%]"
-                onClick={() => {
-                    if (emblaAPI) emblaAPI.scrollNext();
-                }}
-            >
-                <ChevRightIcon width={38} height={38} className="stroke-white" />
-            </button>
+            {!disabled && (
+                <button
+                    type="button"
+                    className="absolute right-2 top-1/2 z-10 -translate-y-1/2 md:right-[2%]"
+                    onClick={() => {
+                        if (emblaAPI) emblaAPI.scrollNext();
+                    }}
+                >
+                    <ChevRightIcon width={38} height={38} className="stroke-white" />
+                </button>
+            )}
         </div>
     );
 }
